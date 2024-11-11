f912d883a0c4a7d5ece6616b6fd53402f13573a4
[Miguel] <11damascomiguel@gmail.com>
2024-11-11 17:43:34 -0300

    primer commit

---
+++ proyecto-gomistar/src/main/java/com/gomistar/proyecto_gomistar/Application.java
@@ -0,0 +1,13 @@
+package com.gomistar.proyecto_gomistar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
